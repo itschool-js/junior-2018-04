eadb418f5cb6ce8b371ece645f5aceb3a68f5a86
Oleg Yasnev <oleg.yasnev@knime.com>
2018-04-10 18:13:28 +0300

    Class 3 - 10.04.2018

--- game.js
+++ game.js
@@ -30,22 +30,38 @@ let planH = plan.length;
 let mageX = 3;
 let mageY = 2;
 
-function printPlan() {
-    let s = '';
+const GRID_SIZE = 20;
+
+function drawPlan() {
+    let planDiv = document.getElementById('plan');
+    planDiv.style.width = planW * GRID_SIZE + 'px';
+    planDiv.style.height = planH * GRID_SIZE + 'px';
+
     for (let i = 0; i < planH; i++) {
         for (let j = 0; j < planW; j++) {
-            if (j == mageX && i == mageY) {
-                s += 'M';
-            } else {
-                s += plan[i][j];
+            if (plan[i][j] == '#') {
+                let wall = document.createElement('div');
+                wall.style.width = GRID_SIZE + 'px';
+                wall.style.height = GRID_SIZE + 'px';
+                wall.style.backgroundColor = 'grey';
+                wall.style.position = 'absolute';
+                wall.style.top = i * GRID_SIZE + 'px';
+                wall.style.left = j * GRID_SIZE + 'px';
+                planDiv.appendChild(wall);
             }
         }
-        s += '\n';
     }
-    console.log(s);
 }
 
-printPlan();
+function drawMage(mage) {
+    let mageDiv = document.getElementById('mage');
+    mageDiv.style.width = GRID_SIZE + 'px';
+    mageDiv.style.height = GRID_SIZE + 'px';
+    mageDiv.style.top = mage.xy.y * GRID_SIZE + 'px';
+    mageDiv.style.left = mage.xy.x * GRID_SIZE + 'px';
+}
+
+//printPlan();
 
 function moveLeft() {
     if (plan[mageY][mageX - 1] == ' ') {
@@ -61,12 +77,95 @@ function moveRight() {
     printPlan();
 }
 
+class Mage {
+    constructor(xy) {
+        this.xy = xy;
+    }
+
+    move(dir) {
+        if (dir.validate()) {
+            let cellxy = new XY(this.xy.x, this.xy.y);
+            cellxy.add(dir);
+            if (plan[cellxy.x][cellxy.y] == ' ') {
+                this.xy = cellxy;
+            }
+        }
+    }
+}
+
+class XY {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    equals(other) {
+        return this.x == other.x && this.y == other.y;
+    }
+
+    add(xy) {
+        this.x += xy.x;
+        this.y += xy.y;
+    }
+}
+
+class Direction extends XY {
+    validate() {
+        return Math.abs(this.x) <= 1 && Math.abs(this.y) <= 1;
+    }
+}
+
+/**
+ * Moves the mage
+ * @param {Direction} dir 
+ */
 function move(dir) {
-    if (Math.abs(dir.x) <= 1 && Math.abs(dir.y) <= 1) {        
+    if (dir.validate()) {
         if (plan[mageY + dir.y][mageX + dir.x] == ' ') {
             mageX += dir.x;
             mageY += dir.y;
         }
     }
-    printPlan();    
 }
+
+function keyMove(mage) {
+    let com = prompt('Make turn: ');
+    com = com.toLowerCase();
+    switch (com) {
+        case 'a':
+            mage.move(new Direction(-1, 0));
+            break;
+        case 's':
+            mage.move(new Direction(0, 1));
+            break;
+        case 'd':
+            mage.move(new Direction(1, 0));
+            break;
+        case 'w':
+            mage.move(new Direction(0, -1));
+            break;
+        case 'q':
+            throw 'Stop the game';
+    }
+    drawMage(mage);
+}
+
+let xy = new XY(1, 2);
+let dir = new Direction(1, 0);
+
+let mage1 = new Mage(xy);
+
+drawPlan();
+drawMage(mage1);
+
+const MAX_TURN = 100;
+let turn = 0;
+function makeTurn() {
+    keyMove(mage1);
+    turn++;
+    if (turn <= MAX_TURN) {
+        setTimeout(makeTurn, 200);
+    }
+}
+
+setTimeout(makeTurn, 0);
